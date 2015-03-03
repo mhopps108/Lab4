600eebaa89a170fe850e25560e9fecfc63531851
mhopps108 <MHopps@dhcp-39-91-225.dhcpint.csulb.edu>
2015-03-03 10:55:43 -0800

    Initial Commit

---
+++ Lab4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Matt Hopps on 3/3/15.
+//  Copyright (c) 2015 Matt Hopps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
